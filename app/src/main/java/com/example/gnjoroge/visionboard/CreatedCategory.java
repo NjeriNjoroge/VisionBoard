941e4cc19649fcc02634ed220b95dcc6a0adf472
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-09-08 15:51:33 +0300

    changed the launcher icon

--- app/src/main/java/com/example/gnjoroge/visionboard/CreatedCategory.java
+++ app/src/main/java/com/example/gnjoroge/visionboard/CreatedCategory.java
@@ -3,15 +3,18 @@ package com.example.gnjoroge.visionboard;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CreatedCategory extends AppCompatActivity {
-
+    private TextView mNewVisionTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_created_category);
         //getting the name and description from CategoriesActivity
+        mNewVisionTextView = (TextView) findViewById(R.id.newVisiontextView);
         Intent intent = getIntent();
         String newVision = intent.getStringExtra("newVision");
+        mNewVisionTextView.setText(newVision);
     }
 }
