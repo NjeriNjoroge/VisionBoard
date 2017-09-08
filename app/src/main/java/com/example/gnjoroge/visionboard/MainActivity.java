3a664e07d06057bed699e858b86715918c956723
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-09-08 12:06:25 +0300

    added a button on the main activity and a new categories activity where users will create new vision board categories

--- app/src/main/java/com/example/gnjoroge/visionboard/MainActivity.java
+++ app/src/main/java/com/example/gnjoroge/visionboard/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.gnjoroge.visionboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mNewVisionBoardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNewVisionBoardButton = (Button) findViewById(R.id.newVisionBoard)
     }
 }
