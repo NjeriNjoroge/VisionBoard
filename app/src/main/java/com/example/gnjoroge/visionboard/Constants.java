20c5a88784b31390a96c8d0e5ca420de55d5bf02
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-09-15 12:41:37 +0300

    created a class to reference the Flickr credentials

---
+++ app/src/main/java/com/example/gnjoroge/visionboard/Constants.java
@@ -0,0 +1,15 @@
+package com.example.gnjoroge.visionboard;
+
+/**
+ * Created by gnjoroge on 9/15/17.
+ */
+
+//file references the Flickr credentials from gradle.properties
+
+public class Constants {
+
+    public static final String FLICKR_CONSUMER_KEY = BuildConfig.FLICKR_CONSUMER_KEY;
+
+    public static final String FLICKR_CONSUMER_SECRET = BuildConfig.FLICKR_CONSUMER_SECRET;
+
+}
