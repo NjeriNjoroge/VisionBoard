7b04febd819825772355448c561c4cbf40d70d0e
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-09-15 16:37:22 +0300

    the api works and is able to log in the logcat the category photos the user wants

--- app/src/main/java/com/example/gnjoroge/visionboard/CategoriesActivity.java
+++ app/src/main/java/com/example/gnjoroge/visionboard/CategoriesActivity.java
@@ -1,5 +1,6 @@
 package com.example.gnjoroge.visionboard;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,27 +8,58 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class CategoriesActivity extends AppCompatActivity {
     private static final String TAG = CategoriesActivity.class.getSimpleName();
-    @Bind(R.id.listView) ListView mListView;
-    private String[] categories = new String[] {"Finances", "Relationships", "Career", "Health", "Travel", "Personal growth"};
+    @Bind(R.id.listView)
+    ListView mListView;
+    private String[] categories = new String[]{"Finances", "Relationships", "Career", "Health", "Travel", "Personal growth"};
+
+
+    private void getPhotos(String category) {
+        final FlickrService flickrService = new FlickrService();
+        flickrService.findPhotos(category, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException{
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
         ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("category");
+        getPhotos(category);
+
         //displaying the categories
-       mListView = (ListView) findViewById(R.id.listView);
+        mListView = (ListView) findViewById(R.id.listView);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, categories);
         mListView.setAdapter(adapter);
 
@@ -35,9 +67,11 @@ public class CategoriesActivity extends AppCompatActivity {
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String categories = ((TextView)view).getText().toString();
+                String categories = ((TextView) view).getText().toString();
                 Toast.makeText(CategoriesActivity.this, categories, Toast.LENGTH_SHORT).show();
             }
         });
     }
-}
+
+
+}//end
--- app/src/main/java/com/example/gnjoroge/visionboard/Constants.java
+++ app/src/main/java/com/example/gnjoroge/visionboard/Constants.java
@@ -1,5 +1,7 @@
 package com.example.gnjoroge.visionboard;
 
+import static android.R.attr.category;
+
 /**
  * Created by gnjoroge on 9/15/17.
  */
@@ -13,7 +15,7 @@ public class Constants {
     public static final String FLICKR_CONSUMER_SECRET = BuildConfig.FLICKR_CONSUMER_SECRET;
 
     //building a request
-    public static final String FLICKR_BASE_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + FLICKR_CONSUMER_KEY + "&text=health";
+    public static final String FLICKR_BASE_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=" + FLICKR_CONSUMER_KEY + "&text";
 
 
     public static final String FLICKR_CATEGORY_QUERY_PARAMETER = "category";
